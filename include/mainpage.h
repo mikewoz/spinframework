1652c3169977c3349a572557f97e82a7192e8bef
Mike Wozniewski <mike@mikewoz.com>
2009-09-11 16:54:19 +0000

    added some doc

--- include/mainpage.h
+++ include/mainpage.h
@@ -69,5 +69,64 @@
 /** @page oscprotocol OSC Protocol
  */
 
+/** @page mscdiagrams Diagrams
+ *
+ * This is a page for diagrams
+ *
+ * \section sec_milhouse Distributed video textures (using Milhouse)
+ *
+ * The following describes how multiple users can instantiate shared video 
+ * textures on SPIN using Milhouse.
+ * 
+ * \msc
+ * 
+
+width=1000;
+spinServer, spinViewer1, milhouseClient1, milhouseClient2;
+
+...;
+--- [label="only spinServer is running"];
+
+...;
+--- [label="spinViewer1 launches"];
+
+spinViewer1->spinServer [label="/spin createNode user01 UserNode"];
+spinServer->* [label="/spin/default createNode user01 UserNode"];
+
+...;
+
+--- [label="milhouseClient1 launches"];
+milhouseClient1->spinServer [label="/spin createNode shvid01 SharedVideoNode\n/spin/shvid01 setParent user01"];
+---;
+spinServer->* [label="/spin/default createNode shvid01 SharedVideoNode\n/spin/shvid01 setParam videoPort 10000\n/spin/shvid01\n/spin/shvid01 start"];
+---;
+---;
+milhouseClient1->milhouseClient1 [label="got /shvid01 start,\nso initiate milhouse SENDER"];
+
+...;
+--- [label="milhouseClient2 launches"];
+milhouseClient2->spinServer [label="/spin/default createNode shvid02 SharedVideoNode"];
+spinServer->* [label="/spin/default createNode shvid02 SharedVideoNode\n/spin/shvid02 setParam videoPort 10020\n/spin/shvid02\n/spin/shvid02 start"];
+---;
+milhouseClient2->milhouseClient2 [label="got /shvid02 start,\nso initiate milhouse SENDER"];
+
+...;
+milhouseClient2->spinServer [label="/spin userRefresh"];
+spinServer->* [label="/spin createNode shvid01 SharedVideoNode\n/spin/shvid01 setParam videoPort 10000\n/spin/shvid01\n/spin/shvid01 start"];
+---;
+milhouseClient2->milhouseClient2 [label="got /shvid01 start,\nso initiate milhouse RECEIVER\n(shmid=shvid01)"];
+
+ *
+ * 
+ * 
+ * \endmsc
+ * 
+ * A a typical invocation of a milhouse receiver will look something like this:
+ * <pre>
+ * milhouse -r --videoport 10000 --address 10.10.10.123 --videocodec mpeg4 --videosink sharedvideosink --shared_memory_id shvid01
+ * </pre>
+ *
+ */
+
 
 #endif
