91a2b65c22479d0fe0ee6a88916bf37e4b68eb41
Tristan Matthews <le.businessman@gmail.com>
2010-07-07 20:51:47 +0000

    camera switching in prototype

--- examples/shared_video/shared_video.cpp
+++ examples/shared_video/shared_video.cpp
@@ -13,52 +13,91 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+    if (argc < 2)
     {
         std::cerr << "Usage: shared_video <path>\n";
         return 1;
     }
 
-	spinClientContext spinListener;
-	spinApp &spin = spinApp::Instance();
+    spinClientContext spinListener;
+    spinApp &spin = spinApp::Instance();
 
-	if (!spinListener.start())
-	{
+    if (!spinListener.start())
+    {
         std::cout << "ERROR: could not start SPIN client thread" << std::endl;
         exit(EXIT_FAILURE);
-	}
+    }
 
-	spin.SceneMessage("sss",
-			"createStateSet",
-			"vid",
-			"SharedVideoTexture",
-			LO_ARGS_END);
-	
     spin.SceneMessage("sss",
-			"createNode",
-			"box",
-			"ShapeNode",
-			LO_ARGS_END);
+            "createStateSet",
+            "vid1",
+            "SharedVideoTexture",
+            LO_ARGS_END);
 
-	spin.NodeMessage("box", "si",
-			"setShape",
-			(int)ShapeNode::BOX,
-			LO_ARGS_END);
+    spin.SceneMessage("sss",
+            "createNode",
+            "box",
+            "ShapeNode",
+            LO_ARGS_END);
 
-	spin.NodeMessage("vid", "ss",
-			"setTextureID",
-			argv[1],
-			LO_ARGS_END);
+    spin.NodeMessage("box", "si",
+            "setShape",
+            (int)ShapeNode::BOX,
+            LO_ARGS_END);
 
-	spin.NodeMessage("box", "ss",
-			"setStateSet",
-			"vid",
-			LO_ARGS_END);
+    spin.NodeMessage("vid1", "ss",
+            "setTextureID",
+            argv[1],
+            LO_ARGS_END);
 
-	std::cout << "\nRunning example. Press CTRL-C to quit..." << std::endl;
+    spin.NodeMessage("box", "ss",
+            "setStateSet",
+            "vid1",
+            LO_ARGS_END);
 
-    while (spinListener.isRunning()) // send signal (eg, ctrl-c to stop)
-        usleep(1000);
+    if (argc > 2)
+    {
+        spin.SceneMessage("sss",
+                "createStateSet",
+                "vid2",
+                "SharedVideoTexture",
+                LO_ARGS_END);
+
+        spin.NodeMessage("vid2", "ss",
+                "setTextureID",
+                argv[2],
+                LO_ARGS_END);
+
+        bool done = false;
+
+        std::cout << "\nRunning example. Press CTRL-C to quit or q to quit." << std::endl;
+        while (!done and spinListener.isRunning())
+        {
+            char c;
+            std::cin >> c;
+            switch (c) {
+                case '1':
+                    spin.NodeMessage("box", "ss",
+                            "setStateSet",
+                            "vid1",
+                            LO_ARGS_END);
+                    break;
+                case '2':
+                    spin.NodeMessage("box", "ss",
+                            "setStateSet",
+                            "vid2",
+                            LO_ARGS_END);
+                    break;
+                case 'q':
+                case 'Q':
+                    done = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
     return 0;
 }
 
