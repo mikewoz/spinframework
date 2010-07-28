5a4b27f5bd4dbeffe2463f781760f8cdaaf165f6
Tristan Matthews <le.businessman@gmail.com>
2010-07-28 18:57:51 +0000

    added support for more video textures in example

--- examples/shared_video/shared_video.cpp
+++ examples/shared_video/shared_video.cpp
@@ -6,45 +6,40 @@
 #include <cstdlib>
 #include <cmath>
 
-#include <stdio.h>
+#include <cstdio>
 #include <termios.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-
-
 /**
- * This example shows how to send commands to a spin server. The result is an
- * orbiting sphere.
+ * This example shows how to send commands to a spin server. 
  */
-
-
 
 int kbhit(void)
 {
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
 
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
 
-  ch = getchar();
+    ch = getchar();
 
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
 
-  if(ch != EOF)
-  {
-    ungetc(ch, stdin);
-    return 1;
-  }
+    if(ch != EOF)
+    {
+        ungetc(ch, stdin);
+        return 1;
+    }
 
-  return 0;
+    return 0;
 }
 
 
@@ -52,9 +47,9 @@ int kbhit(void)
 
 int main(int argc, char **argv)
 {
-    if (argc != 4)
+    if (argc < 4)
     {
-        std::cerr << "Usage: shared_video <server_name> <shared_video1_path> <shared_video2_path>\n";
+        std::cerr << "Usage: shared_video <server_name> <shared_video1_path>...<shared_video4_path>\n";
         return 1;
     }
 
@@ -86,12 +81,29 @@ int main(int argc, char **argv)
             "vid1",
             "SharedVideoTexture",
             LO_ARGS_END);
-    
+
     spin.SceneMessage("sss",
             "createStateSet",
             "vid2",
             "SharedVideoTexture",
             LO_ARGS_END);
+
+    if (argc > 4)
+    {
+        spin.SceneMessage("sss",
+                "createStateSet",
+                "vid3",
+                "SharedVideoTexture",
+                LO_ARGS_END);
+    }
+    if (argc > 5)
+    {
+        spin.SceneMessage("sss",
+                "createStateSet",
+                "vid4",
+                "SharedVideoTexture",
+                LO_ARGS_END);
+    }
 
     // set textureID on stateset
     spin.NodeMessage("vid1", "ss",
@@ -104,12 +116,28 @@ int main(int argc, char **argv)
             argv[3],
             LO_ARGS_END);
 
-    // apply the stateset to the box
+    if (argc > 4)
+    {
+        spin.NodeMessage("vid3", "ss",
+                "setTextureID",
+                argv[4],
+                LO_ARGS_END);
+
+    }
+    if (argc > 5)
+    {
+        spin.NodeMessage("vid4", "ss",
+                "setTextureID",
+                argv[5],
+                LO_ARGS_END);
+
+    }
+
+    // apply a stateset to the box
     spin.NodeMessage("box", "ss",
             "setStateSet",
             "vid1",
             LO_ARGS_END);
-
 
     usleep(1000);
 
@@ -135,6 +163,26 @@ int main(int argc, char **argv)
                             "setStateSet",
                             "vid2",
                             LO_ARGS_END);
+                    break;
+                case '3':
+                    if (argc > 4)
+                    {
+                        std::cout << "texture3" << std::endl;
+                        spin.NodeMessage("box", "ss",
+                                "setStateSet",
+                                "vid3",
+                                LO_ARGS_END);
+                    }
+                    break;
+                case '4':
+                    if (argc > 5)
+                    {
+                        std::cout << "texture4" << std::endl;
+                        spin.NodeMessage("box", "ss",
+                                "setStateSet",
+                                "vid4",
+                                LO_ARGS_END);
+                    }
                     break;
                 case 'q':
                 case 'Q':
