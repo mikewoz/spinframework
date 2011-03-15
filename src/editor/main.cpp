c12c1a10134d0db7bf15298387d52b9198dbe46e
Alexandre Quessy <alexandre@quessy.net>
2011-03-15 16:57:08 -0400

    list all spin nodes at startup

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -1,43 +1,22 @@
-// -----------------------------------------------------------------------------
-// |    ___  ___  _  _ _     ___                                        _      |
-// |   / __>| . \| || \ |   | __>_ _  ___ ._ _ _  ___  _ _ _  ___  _ _ | |__   |
-// |   \__ \|  _/| ||   |   | _>| '_><_> || ' ' |/ ._>| | | |/ . \| '_>| / /   |
-// |   <___/|_|  |_||_\_|   |_| |_|  <___||_|_|_|\___.|__/_/ \___/|_|  |_\_\   |
-// |                                                                           |
-// |---------------------------------------------------------------------------|
-//
-// http://spinframework.sourceforge.net
-// Copyright (C) 2009 Mike Wozniewski, Zack Settel
-//
-// Developed/Maintained by:
-//    Mike Wozniewski (http://www.mikewoz.com)
-//    Zack Settel (http://www.sheefa.net/zack)
-//
-// Principle Partners:
-//    Shared Reality Lab, McGill University (http://www.cim.mcgill.ca/sre)
-//    La Societe des Arts Technologiques (http://www.sat.qc.ca)
-//
-// Funding by:
-//    NSERC/Canada Council for the Arts - New Media Initiative
-//    Heritage Canada
-//    Ministere du Developpement economique, de l'Innovation et de l'Exportation
-//
-// -----------------------------------------------------------------------------
-//  This file is part of the SPIN Framework.
-//
-//  SPIN Framework is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  SPIN Framework is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public License
-//  along with SPIN Framework. If not, see <http://www.gnu.org/licenses/>.
-// -----------------------------------------------------------------------------
+/*
+* This file is part of the SPIN Framework.
+*
+* Copyright (c) 2009 Mike Wozniewski
+* Copyright (c) 2009 Zack Settel
+*
+* SPIN Framework is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* SPIN Framework is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+* 
+* You should have received a copy of the GNU Lesser General Public License
+* along with SPIN Framework. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifdef WX_PRECOMP
 #include "wx_pch.h"
@@ -46,8 +25,61 @@
 #ifdef __BORLANDC__
 #pragma hdrstop
 #endif //__BORLANDC__
+
 #include "main.h"
+#include "unused.h"
+
 #include <wx/msgdlg.h>
+
+#include <cppintrospection/Attributes>
+#include <cppintrospection/Exceptions>
+#include <cppintrospection/ExtendedTypeInfo>
+#include <cppintrospection/MethodInfo>
+#include <cppintrospection/PropertyInfo>
+#include <cppintrospection/Reflection>
+#include <cppintrospection/ReflectionMacros>
+#include <cppintrospection/StaticMethodInfo>
+#include <cppintrospection/Type>
+#include <cppintrospection/TypedMethodInfo>
+#include <cppintrospection/Value>
+#include <cppintrospection/variant_cast>
+#include <vector>
+#include <string>
+#include <iostream>
+
+namespace spin
+{
+namespace editor
+{
+std::vector<std::string> listSpinNodeTypes()
+{
+    namespace intro = cppintrospection;
+    std::vector<std::string> ret;
+    try
+    {
+        const intro::Type &ReferencedNodeType = cppintrospection::Reflection::getType("ReferencedNode");
+        const intro::TypeMap &allTypes = cppintrospection::Reflection::getTypes();
+        cppintrospection::TypeMap::const_iterator it;
+        for (it = allTypes.begin(); it != allTypes.end(); ++it)
+        {
+            if (((*it).second)->isDefined())
+            {
+                if ( ((*it).second)->isSubclassOf(ReferencedNodeType) )
+                {
+                    std::string theType = ((*it).second)->getName();
+                    ret.push_back(theType);
+                }
+            }
+        }
+    }
+    catch (const intro::Exception &ex)
+    {
+        std::cerr << __FUNCTION__ << ": Could not list node types:\n" << ex.what() << std::endl;
+    }
+    return ret;
+}
+} // end of namespace editor
+} // end of namespace spin
 
 //helper functions
 enum wxbuildinfoformat
@@ -83,27 +115,36 @@ spineditorFrame::spineditorFrame(wxFrame *frame) :
     GUIFrame(frame)
 {
 #if wxUSE_STATUSBAR
-    statusBar->SetStatusText(_("Hello Code::Blocks user!"), 0);
+    statusBar->SetStatusText(_("Welcome to the SPIN Editor!"), 0);
     statusBar->SetStatusText(wxbuildinfo(short_f), 1);
 #endif
+    std::vector<std::string> nodeTypes = spin::editor::listSpinNodeTypes();
+    std::cout << "SPIN Node types:" << std::endl;
+    for (std::vector<std::string>::iterator iter = nodeTypes.begin(); iter != nodeTypes.end(); ++iter)
+        std::cout << " * " << *iter << std::endl;
 }
 
 spineditorFrame::~spineditorFrame()
 {
 }
 
+
 void spineditorFrame::OnClose(wxCloseEvent &event)
 {
+    UNUSED(event);
     Destroy();
 }
 
 void spineditorFrame::OnQuit(wxCommandEvent &event)
 {
+    UNUSED(event);
     Destroy();
 }
 
 void spineditorFrame::OnAbout(wxCommandEvent &event)
 {
+    UNUSED(event);
     wxString msg = wxbuildinfo(long_f);
     wxMessageBox(msg, _("Welcome to..."));
 }
+
