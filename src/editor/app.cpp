cf3ec7d9b262a2f03db29f351d1d397724bc1d65
Mike Wozniewski <mike@mikewoz.com>
2011-03-15 16:14:53 -0400

    towards 1st wx example

--- src/editor/app.cpp
+++ src/editor/app.cpp
@@ -1,11 +1,43 @@
-/***************************************************************
- * Name:      spineditorApp.cpp
- * Purpose:   Code for Application Class
- * Author:    Mike Wozniewski (mike@mikewoz.com)
- * Created:   2011-03-15
- * Copyright: Mike Wozniewski (http://www.mikewoz.com)
- * License:
- **************************************************************/
+// -----------------------------------------------------------------------------
+// |    ___  ___  _  _ _     ___                                        _      |
+// |   / __>| . \| || \ |   | __>_ _  ___ ._ _ _  ___  _ _ _  ___  _ _ | |__   |
+// |   \__ \|  _/| ||   |   | _>| '_><_> || ' ' |/ ._>| | | |/ . \| '_>| / /   |
+// |   <___/|_|  |_||_\_|   |_| |_|  <___||_|_|_|\___.|__/_/ \___/|_|  |_\_\   |
+// |                                                                           |
+// |---------------------------------------------------------------------------|
+//
+// http://spinframework.sourceforge.net
+// Copyright (C) 2009 Mike Wozniewski, Zack Settel
+//
+// Developed/Maintained by:
+//    Mike Wozniewski (http://www.mikewoz.com)
+//    Zack Settel (http://www.sheefa.net/zack)
+//
+// Principle Partners:
+//    Shared Reality Lab, McGill University (http://www.cim.mcgill.ca/sre)
+//    La Societe des Arts Technologiques (http://www.sat.qc.ca)
+//
+// Funding by:
+//    NSERC/Canada Council for the Arts - New Media Initiative
+//    Heritage Canada
+//    Ministere du Developpement economique, de l'Innovation et de l'Exportation
+//
+// -----------------------------------------------------------------------------
+//  This file is part of the SPIN Framework.
+//
+//  SPIN Framework is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  SPIN Framework is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with SPIN Framework. If not, see <http://www.gnu.org/licenses/>.
+// -----------------------------------------------------------------------------
 
 #ifdef WX_PRECOMP
 #include "wx_pch.h"
@@ -14,17 +46,17 @@
 #ifdef __BORLANDC__
 #pragma hdrstop
 #endif //__BORLANDC__
+#include "app.h"
+#include "main.h"
 
-#include "spineditorApp.h"
-#include "spineditorMain.h"
-
-IMPLEMENT_APP(spineditorApp);
+IMPLEMENT_APP( spineditorApp )
+;
 
 bool spineditorApp::OnInit()
 {
     spineditorFrame* frame = new spineditorFrame(0L);
-    
+
     frame->Show();
-    
+
     return true;
 }
