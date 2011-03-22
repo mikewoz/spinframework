8bbcf2239ff8b341e418487d5035140c4ff6245a
Alexandre Quessy <alexandre@quessy.net>
2011-03-22 10:11:08 -0400

    updated headers

--- src/editor/wxSpinTreeCtrl.h
+++ src/editor/wxSpinTreeCtrl.h
@@ -1,49 +1,27 @@
-// -----------------------------------------------------------------------------
-// |    ___  ___  _  _ _     ___                                        _      |
-// |   / __>| . \| || \ |   | __>_ _  ___ ._ _ _  ___  _ _ _  ___  _ _ | |__   |
-// |   \__ \|  _/| ||   |   | _>| '_><_> || ' ' |/ ._>| | | |/ . \| '_>| / /   |
-// |   <___/|_|  |_||_\_|   |_| |_|  <___||_|_|_|\___.|__/_/ \___/|_|  |_\_\   |
-// |                                                                           |
-// |---------------------------------------------------------------------------|
-//
-// http://spinframework.sourceforge.net
-// Copyright (C) 2009 Mike Wozniewski, Zack Settel
-//
-// Developed/Maintained by:
-//    Mike Wozniewski (http://www.mikewoz.com)
-//    Zack Settel (http://www.sheefa.net/zack)
-//
-// Principle Partners:
-//    Shared Reality Lab, McGill University (http://www.cim.mcgill.ca/sre)
-//    La Societe des Arts Technologiques (http://www.sat.qc.ca)
-//
-// Funding by:
-//    NSERC/Canada Council for the Arts - New Media Initiative
-//    Heritage Canada
-//    Ministere du Developpement economique, de l'Innovation et de l'Exportation
-//
-// -----------------------------------------------------------------------------
-//  This file is part of the SPIN Framework.
-//
-//  SPIN Framework is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  SPIN Framework is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public License
-//  along with SPIN Framework. If not, see <http://www.gnu.org/licenses/>.
-// -----------------------------------------------------------------------------
-//
-//  NOTE: This file is based on source code from the Orihalcon Framework Library
-//  Copyright (C) 2005 by Toshiyuki Takahei <takahei@orihalcon.jp>
-//  (Released under the GNU Lesser General Public License)
-//
-// -----------------------------------------------------------------------------
+/*
+ * This file is part of the SPIN Framework.
+ *
+ * Copyright (c) 2009 Mike Wozniewski
+ * Copyright (c) 2009 Zack Settel
+ * Copyright (c) 2011 Alexandre Quessy
+ *
+ * SPIN Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SPIN Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SPIN Framework. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** \file
+ * The wxSpinTreeCtrl class.
+ */
 
 #ifndef _wxSpinTreeCtrl_H_
 #define _wxSpinTreeCtrl_H_
