e45120125ea52cdb3e3401826bf12d42726adeab
Emmanuel Durand <emmanueldurand@gmail.com>
2012-08-10 14:23:24 -0400

    Corrected some artifacts that were still in the DoF PPU. Some very small artifacts are still visible with some settings.

--- src/Resources/shaders/dof_gauss_1Dx_fp.glsl
+++ src/Resources/shaders/dof_gauss_1Dx_fp.glsl
@@ -11,6 +11,8 @@ uniform float radius;
 varying float sigma2;
 varying float c;
 
+const float epsilon = 0.01;
+
 void main(void)
 {
     vec4 lColor = vec4(0.0);
@@ -25,13 +27,14 @@ void main(void)
     for(float i=-lRadius; i<lRadius; i+=1.0)
     {
         float lWeight = c*exp(-(i*i)/sigma2);
+        vec2 lPixCoords = gl_TexCoord[0].st + vec2(i*lInputTexTexelWidth, 0);
         
         // Radius of the blur created by the current offset pixel
-        //float lPixBlurRadius = radius * texture2D(texBlurMap, gl_TexCoord[0].st + vec2(i*lInputTexTexelWidth, 0)).r;
+        float lPixBlurRadius = radius * texture2D(texBlurMap, lPixCoords).r;
 
-        //float lIsInBlur = 1.0; //step(lPixBlurRadius, lRadius);
-        lTotalWeight += lWeight;
-        lColor += texture2D(texColorMap, gl_TexCoord[0].st + vec2(i*lInputTexTexelWidth, 0)) * lWeight;
+        float lIsInBlur = step(abs(i)-epsilon, lPixBlurRadius);
+        lTotalWeight += lWeight * lIsInBlur;
+        lColor += lIsInBlur * texture2D(texColorMap, lPixCoords) * lWeight;
     }
     lColor /= lTotalWeight;
 
--- src/Resources/shaders/dof_gauss_1Dy_fp.glsl
+++ src/Resources/shaders/dof_gauss_1Dy_fp.glsl
@@ -11,11 +11,13 @@ uniform float radius;
 varying float sigma2;
 varying float c;
 
+const float epsilon = 0.01;
+
 void main(void)
 {
     vec4 lColor = vec4(0.0);
     float lTotalWeight = 0.0;
-    float lInputTexTexelWidth = 1.0 / osgppu_ViewportHeight;
+    float lInputTexTexelHeight = 1.0 / osgppu_ViewportHeight;
 
     // Get the blur value at the pixel level
     float lBlur = texture2D(texBlurMap, gl_TexCoord[0].st).r;
@@ -25,13 +27,14 @@ void main(void)
     for(float i=-lRadius; i<lRadius; i+=1.0)
     {
         float lWeight = c*exp(-(i*i)/sigma2);
+        vec2 lPixCoords = gl_TexCoord[0].st + vec2(0, i*lInputTexTexelHeight);
         
         // Radius of the blur created by the current offset pixel
-        //float lPixBlurRadius = radius * texture2D(texBlurMap, gl_TexCoord[0].st + vec2(0, i*lInputTexTexelWidth)).r;
+        float lPixBlurRadius = radius * texture2D(texBlurMap, lPixCoords).r;
 
-        //float lIsInBlur = 1.0; //step(lPixBlurRadius, lRadius);
-        lTotalWeight += lWeight;
-        lColor += texture2D(texColorMap, gl_TexCoord[0].st + vec2(0, i*lInputTexTexelWidth)) * lWeight;
+        float lIsInBlur = step(abs(i)-epsilon, lPixBlurRadius);
+        lTotalWeight += lWeight * lIsInBlur;
+        lColor += lIsInBlur * texture2D(texColorMap, lPixCoords) * lWeight;
     }
     lColor /= lTotalWeight;
 
