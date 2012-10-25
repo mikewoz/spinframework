d436ba23dffba2431681668ebb692beb88cf6bb6
Alexandre Quessy <alexandre@quessy.net>
2012-10-25 11:01:43 -0400

    fix indentation and do not use namespace in orbit

--- examples/orbit/orbit.cpp
+++ examples/orbit/orbit.cpp
@@ -1,7 +1,7 @@
-#include <spinFramework/spinUtil.h>
-#include <spinFramework/spinApp.h>
-#include <spinFramework/spinClientContext.h>
-#include <spinFramework/ShapeNode.h>
+//#include <spinFramework/spinUtil.h>
+#include <spin/spinApp.h>
+#include <spin/spinClientContext.h>
+#include <spin/ShapeNode.h>
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
@@ -13,49 +13,47 @@
 
 int main(int argc, char **argv)
 {
-    using namespace spin;
-    
-    spinClientContext spinListener;
-	spinApp &spin = spinApp::Instance();
+    spin::spinClientContext spinListener;
+    spin::spinApp &spin_app = spin::spinApp::Instance();
 
-	if (!spinListener.start())
-	{
+    if (! spinListener.start())
+    {
         std::cout << "ERROR: could not start SPIN client thread" << std::endl;
         exit(EXIT_FAILURE);
-	}
+    }
 
-	spin.SceneMessage("sss",
-			"createNode",
-			"shp",
-			"ShapeNode",
-			LO_ARGS_END);
+    spin_app.SceneMessage("sss",
+            "createNode",
+            "shp",
+            "ShapeNode",
+            LO_ARGS_END);
 
-	spin.NodeMessage("shp", "si",
-			"setShape",
-			(int)ShapeNode::SPHERE,
-			LO_ARGS_END);
+    spin_app.NodeMessage("shp", "si",
+            "setShape",
+            (int)spin::ShapeNode::SPHERE,
+            LO_ARGS_END);
 
-	float orbitRadius = 2.0;
-	double orbitDuration = 2.0;
-	int numSamples = 100;
+    float orbitRadius = 2.0;
+    double orbitDuration = 2.0;
+    int numSamples = 100;
 
-	std::cout << "\nRunning example. Press CTRL-C to quit..." << std::endl;
+    std::cout << "\nRunning example. Press CTRL-C to quit..." << std::endl;
 
     while (spinListener.isRunning()) // send signal (eg, ctrl-c to stop)
     {
-		for (int i = 0; i < numSamples; ++i)
-		{
-			float angle = i * 2.0f*osg::PI/((float)numSamples-1.0f);
+        for (int i = 0; i < numSamples; ++i)
+        {
+            float angle = i * 2.0f*osg::PI/((float)numSamples-1.0f);
 
-			spin.NodeMessage("shp", "sfff",
-					"setTranslation",
-					sinf(angle)*orbitRadius,
-					cosf(angle)*orbitRadius,
-					0.0,
-					LO_ARGS_END);
+            spin_app.NodeMessage("shp", "sfff",
+                    "setTranslation",
+                    sinf(angle)*orbitRadius,
+                    cosf(angle)*orbitRadius,
+                    0.0,
+                    LO_ARGS_END);
 
-			usleep(1000000 * orbitDuration / numSamples);
-		}
+            usleep(1000000 * orbitDuration / numSamples);
+        }
     }
     return 0;
 }
